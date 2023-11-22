5a446158e5b132dcd78077035fe0ef3ab4d199ff
Dalpat singh <dalpat.singh@microprixs.in>
2023-11-22 19:17:04 +0530

    design chnages

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -186,8 +186,8 @@
     <!-- New Section  -->
     <section>
       <div class="grid lg:grid-cols-2 lg:mt-16 mt-32 mb-10 lg:min-h-[800px] min-h-[900px]">
-        <div class="flex relative flex-col justify-center flex-nowrap  lg:min-w-[52rem] ">
-          <div class="bg-[#f9c9ce] min-h-[190px] flex lg:justify-center-items lg:justify-center justify-end items-center xl:max-w-[880px] lg:max-w-[800px] md:max-w-[800px] sm:max-w-[650px] max-w-[650px]">     
+        <div class="flex relative flex-col justify-center flex-nowrap  lg:min-w-[52rem] sm:pr-0 pr-6">
+          <div class="bg-[#f9c9ce] min-h-[190px] flex lg:justify-center-items lg:justify-center justify-end items-center xl:max-w-[880px] lg:max-w-[800px] md:max-w-[800px] sm:max-w-[650px] max-w-[650px] ">     
             <div class="bg-cover bg-center absolute md:min-h-[360px] md:w-[380px] lg:w-[280px] w-[320px] h-[400px] border-4 border-white lg:ml-0 lg:ml-0 lg:mr-[0px] mb-[14px]" style="background-image: url('https://images02.nicepage.com/c461c07a441a5d220e8feb1a/6d35051f156a5a558e14089e/v.jpg?version=;')"></div>            
           </div> 
           <div class="flow self-center xl:pl-10 lg:pl-0 xl:pr-0 lg:pr-20 md:pl-24 lg:mt-10 lg:mt-10">
@@ -221,8 +221,8 @@
         </div>  
         
         <!-- 2nd Section -->
-        <div class="md:items-center md:flex lg:mt-20 mt-10 flex lg:justify-center xl:justify-start sm:justify-center md:justify-end">
-          <div class="bg-[#2530c9] text-white lg:w-[470px] md:w-[400px] w-[300px] absolute px-8 lg:mx-10 xl:mx-0 lg:py-10 py-4 lg:mr-0 md:mr-32 mr-0">
+        <div class="md:items-center md:flex lg:mt-20 mt-10 flex   lg:justify-start sm:justify-center md:justify-end  sm:pl-0  pl-12">
+          <div class="bg-[#2530c9] text-white lg:w-[470px] md:w-[400px] sm:w-[380px] w-[300px] absolute px-8 lg:mx-10 xl:mx-0 lg:py-10 py-4 lg:mr-0 md:mr-60">
             <p class="lg:text-4xl md:text-3xl text-xl font-semibold md:py-4" >BRANDING & DESIGN</p>
             <p class="lg:text-[1.050rem] lg:py-4 py-2 text-small">Quam nulla porttitor massa id neque aliquam vestibulum morbi. Eu consequat ac felis donec et odio pellentesque. Turpis nunc eget lorem dolor sed. Ornare quam viverra orci sagittis eu volutpat odio. Sed vulputate odio ut enim blandit volutpat.</p>
             <p class="lg:py-8 py-4 text-lg">Images from <a class="underline underline-offset-8 text-white cursor-pointer hover:bg-white "> Freepik</a></p>
@@ -230,8 +230,8 @@
             <p class="bg-[#f49ea8] hover:bg-[#259bca]  text-white px-10 py-3 font-bold text-lg rounded-full tracking-widest">LEARN MORE</p>
           </div>
           </div>
-          <div class=" flex self-end lg:ml-[110px] bottom-0 right-0">
-            <img class="lg:min-h-[300px] min-h-[262px] lg:mt-0 mt-36 md:ml-0 ml-20 lg:w-[650px] md:w-[450px]  w-[260px] object-[44% 50%] object-cover" src="https://images02.nicepage.com/c461c07a441a5d220e8feb1a/6baa3038983251ae8c7607a8/yhjcg.jpg"/>
+          <div class=" flex self-end lg:pl-[110px] md:pl-96 sm:pl-72 pl-20">
+            <img class="lg:min-h-[300px] min-h-[262px] lg:mt-0 mt-36 md:ml-0 dm:ml-0 sm:pl-0 pl-0 xl:max-w-6/12 lg:max-w-6/12  md:max-w-5/12 sm:max-w-5/12 max-w-full object-[44% 50%] object-cover" src="https://images02.nicepage.com/c461c07a441a5d220e8feb1a/6baa3038983251ae8c7607a8/yhjcg.jpg"/>
           </div>
         </div>
       </div>
@@ -244,7 +244,7 @@
 
     <!-- third section -->
       <section class="lg:min-h-[850px]  md:min-h-[700px] min-h-[1020px] bg-[#f9c9ce] ">
-        <div class=" md:pt-16 lg:p-6 xl-10 pt-20 sm:p-8 p-8 relative " > 
+        <div class=" md:pt-16 lg:p-8 lg:pt-16 xl-10 pt-20 sm:p-8 p-8 relative " > 
           <div class="w-52 color-white absolute mt-[-1rem] xl:left-20 left-0">
             <svg  xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" xml:space="preserve" class="u-svg-content fill-white" viewBox="0 0 160 160" x="0px" y="0px" id="svg-0a30"><path d="M114.3,152.3l38-38C144.4,130.9,130.9,144.4,114.3,152.3z M117.1,9.1l-108,108c0.8,1.6,1.7,3.2,2.7,4.8l110-110
             C120.3,10.9,118.7,10,117.1,9.1z M97.5,2L2,97.5c0.4,2,1,4,1.5,5.9l99.9-99.9C101.5,2.9,99.5,2.4,97.5,2z M80,160c2,0,4-0.1,5.9-0.2
@@ -297,14 +297,14 @@
       <section id="project" class="">
         <div class="sm:grid grid-cols-1 xl:grid-cols-3 lg:grid-cols-3 md:grid-cols-3 sm:grid-cols-1 md:pb-0 pb-10">
           <div class="w-full col-span-2 py-10 font-sans">
-            <div class="sm:pl-24">
+            <div class="lg:pl-16 xl:pl-24 sm:pl-24 pl-5">
               <p class="text-xl font-normal text-black my-4">WHAT WE DO</p>
               <h1 class="text-[#2530c9] sm:text-5xl font-semibold my-4 text-4xl">About Us & Our Work</h1>
               <p class="text-[1.115rem] text-black xl:pr-64 sm:pr-10 md:pr-0 lg:pr-20 pr-10">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
             </div>
             <div class=" grid grid-cols-1 pt-8" >
               <div>
-              <div class=" text-3xl text-black sm:pl-24" >
+              <div class=" text-3xl text-black lg:pl-16 xl:pl-24 sm:pl-24 pl-5" >
                 <i class="fi fi-rs-angle-right"></i>
               </div>
              </div>
@@ -326,7 +326,7 @@
           </div>
         </div>
         <div class=" md:self-center sm:absolute absolute xl:mt-32 lg:mt-32 md:mt-40  mt-0 sm:grid grid-cols-3  relative self-end">
-          <p class="w-full text-black text-[1.125rem]  xl:pl-24 lg:pr-5 md:pl-24 sm:mt-32 sm:px-0 px-10" >Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+          <p class="w-full text-black text-[1.125rem]  xl:pl-24 lg:pr-10 md:pl-24 sm:mt-32 sm:px-0 px-10" >Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
         </div>
       </div>     
       </section>
@@ -392,31 +392,31 @@
               </div>
             </div>
           </div>
-          <div class="md:p-12 sm:p-2 lg:text-start text-center ">
+          <div class="md:p-12 sm:p-2 lg:text-start text-center sm:pt-0 pt-8 ">
             <div class="pb-8 sm:">
               <p class="text-[#fc1aaf] text-3xl font-semibold">Address</p>
               <p class="text-black xl:pr-40 lg:pr-20 pt-4 md:pr-10  md:text-base sm:text-lg sm:pr-10 sm:px-0 px-12">1600 Amphitheatre Parkway, Mountain View, CA 94043</p>
             </div>
             <div class="pb-8">
               <p class="text-[#fc1aaf] text-3xl font-semibold">Phone</p>
-              <p class="text-black lg:pr-32 pt-4 text-base sm:pr-0">+321 123 4567</p>
+              <a href="tel:998-249-9155" class="text-black lg:pr-32 pt-4 text-base sm:pr-0  hover:bg-[#ffffff00]">+91 998 249 9155</a>
             </div>
             <div class="pb-8">
               <p class="text-[#fc1aaf] text-3xl font-semibold">Email</p>
-              <p class="pt-4 font-bold text-[#2530c9] underline underline-offset-4 text-lg ">info@example.com</p>
+              <a href="mailto:lokendrasingh07773@email.com" class="pt-4 font-bold text-[#2530c9] underline underline-offset-4 text-lg hover:bg-[#ffffff00]">@example.com</a>
             </div>
 
             <div class="sm:grid sm:justify-items-center lg:justify-items-start md:justify-items-center xl:justify-items-start grid justify-items-center mb-10">
               <div class="min-h-[1px] w-3/6 bg-[#5ca4fb] mt-16"></div>
               <div class="pt-4 px-4">
-                <i class="fi fi-brands-facebook   text-[#3B5998] text-3xl pr-4"></i>
-                <i class="fi fi-brands-twitter    text-[#55ACEE] text-3xl pr-4"></i>
-                <i class="fi fi-brands-instagram  text-[#C536A4] text-3xl pr-4"></i>
-                <i class="fi fi-brands-youtube    text-[#D22215] text-3xl pr-4"></i>
+                <a target="blank" class="hover:bg-white" href="https://www.facebook.com/lokendra.singhmertiya.589?mibextid=2JQ9oc"><i class="fi fi-brands-facebook text-[#3B5998] text-3xl pr-4"></i></a>                
+                <a target="blank" class="hover:bg-white" href="https://www.instagram.com/lokendra_banna7773"><i class="fi fi-brands-instagram  text-[#C536A4] text-3xl pr-4"></i></a>
+                <a target="blank" class="hover:bg-white" href="https://www.twitter.com/LokendraBanna19"><i class="fi fi-brands-twitter text-[#03a9f4] text-3xl pr-4"></i></a>
+                <a target="blank" class="hover:bg-white" href="https://wa.me/9982499155"><i class="fi fi-brands-whatsapp  text-[#39ae41] text-3xl pr-4"></i></a>
               </div>
             </div>
           </div>
-        </div>
+        </div><i class=""></i>
       </section>
 
 
@@ -439,8 +439,8 @@
       </section>
 
       <section class="bg-black min-h-[75px] flex justify-center items-center">
-        <div class="text-white">
-          <p> @copyright Microprixs Solution Pvt. Ltd</p>
+        <div >
+          <a target="blank" href="https://www.microprixs.com" class="text-white  hover:bg-[#ffffff00] "> @copyright Microprixs Solution Pvt. Ltd</a>          
         </div>
       </section>
 </template>
@@ -448,6 +448,55 @@
 <style>
 .interiorDesign{
   background-image: url('./../assets/images/aser-min.jpg');
+
+}
+ .font-sans.bg-\[\#2530c9\].lg\:absolute.md\:relative.sm\:p-8.p-8.sm\:py-10.align-bottom.shadow-2xl.shadow-indigo-500\/50.sm\:block.sm\:p-0.sm\:py-0{
+  animation: MyDiv;
+  animation-duration: 1s;
+  animation-iteration-count: 1;
+  animation-timing-function: ease-in;
+  animation-direction: normal;
+ }
+ @keyframes MyDiv {
+  0%   {left: 0px; }
+  100%  {left: 300px; }
+}
+
+.font-sans.bg-\[\#3e427e\].lg\:absolute.sm\:p-8.p-8.sm\:py-10.align-bottom.shadow-2xl.shadow-indigo-500\/50.sm\:block{
+  animation: MyDiv1;
+  animation-duration: 1s;
+  animation-iteration-count: 1;
+  animation-timing-function: ease-in;
+  animation-direction: normal;
+}
+
+@keyframes MyDiv1 {
+  0%   {right: 0px; }
+  100%  {right: 300px; }
+}
+
+.flex.relative.flex-col.justify-center.gap-12.px-4.md\:mt-32.md\:pb-16.sm\:pb-48.leading-tight.text-white.md\:justify-self-center.min-w-\[250px\].md\:pb-0.sm\:pb-40.pb-4.md\:px-0.sm\:px-40.px-10{
+  animation: MyDiv2;
+  animation-duration: 2s;
+  animation-iteration-count: 1;
+  animation-timing-function: ease-in;
+  animation-direction: normal;
+}
+@keyframes MyDiv2 {
+  0%   {right: -340px; }
+  100%  {right: 0px; }
+}
+
+.bg-white.md\:w-full.text-black.py-16.px-8.shadow-2xl{
+  animation: MyDiv_a;
+  animation-duration: 2s;
+  animation-iteration-count: 1;
+  animation-timing-function: ease-in;
+  animation-direction: normal;
+}
+@keyframes MyDiv_a {
+  0%   {right: 0px; }
+  100%  {right: 340px; }
 }
 </style>
 
